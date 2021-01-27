c322e8ccc48da9c8ff05f1e59ea94b84aeecb839
ivoad <77401440+ivoad@users.noreply.github.com>
2021-01-27 15:39:40 +0000

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -3,5 +3,37 @@ namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
     export const transparency16 = image.ofBuffer(hex``);
 
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`1000100002020202020202020202020202020202020000010000000000000000000000020200000100000002000000000100000202000101000000000000000001010002020000000001010001010000020000020202000000000000000000000000000202000001000000000200000000000002020202020202020202020202020202020202020202020202020202020202020202010001000002020100020202020202020000000002020202020202020100020202020202020202020202020202020202000202020202020202020202020202020202020202020200010002020200020200010001000001000100000001000202020202020202020202020202020202`, img`
+. . . . . . . . . . . . 2 . . . 
+. . . 2 . . . . . . . . . . . . 
+. . . 2 . . . . . . . . 2 . . . 
+. . 2 2 . . . . . . . . 2 2 . . 
+. . . . . 2 2 . 2 2 . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . 2 . . . . . . . . . . . . 
+. . . . . . . . 2 . . . . . . . 
+. . . 2 . . 2 2 2 . . . . . . . 
+. 2 . 2 . . 2 2 2 . . . . . . . 
+. . . . . . . . . . . 2 2 2 . . 
+. . . . . . . . . 2 2 . . . . . 
+. . 2 . . . . . . 2 . . . . . . 
+. . . . . . . . . 2 . . . . . . 
+. . 2 . 2 . . 2 . 2 . . . 2 . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,sprites.castle.rock2,sprites.castle.shrub], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+        }
+        return null;
+    })
+
 }
 // Auto-generated code. Do not edit.
